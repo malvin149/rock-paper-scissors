be233e45c0d12f3c00a6ad0cc3f8659973fd6df0
malvin149 <205408852+malvin149@users.noreply.github.com>
2025-06-29 18:00:11 +0000

    Create a fucntion to return computer choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,23 @@
-console.log('Hello World');
+/* 
+create a function called getComputerChoice to return either 
+rock, paper, or scissors.
+*/
+const getComputerChoice = () => {
+    // generate random number from 0 to 2
+    let computerChoice = Math.random()* 3;
+    // round up to whole number
+    computerChoice = Math.floor(computerChoice)
+    if (computerChoice == 0) {
+        // if random number is 0 return rock
+        computerChoice = 'rock';
+    } else if (computerChoice == 1) {
+        // if random number is 0 return paper
+        computerChoice = 'paper';
+    } else {
+        // else return scissors, the last choice 
+        computerChoice = 'scissors';
+    }
+    console.log(computerChoice);
+}
+
+getComputerChoice();
