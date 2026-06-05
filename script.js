f35260fe1601e0e823598c0bcd7c247167bc8646
malvin149 <205408852+malvin149@users.noreply.github.com>
2026-06-05 16:25:40 +0000

    Create getHumanChoice and playRound functions with input validation

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // console.log('Hello World');
+let computerScore = 0
+let humanScore = 0
 
 const getComputerChoice = () => {
     let computerChoice = Math.floor(Math.random() * 3);
@@ -8,8 +10,32 @@ const getComputerChoice = () => {
     if (computerChoice === 2) return 'scissors';
 };
 
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
+const getHumanChoice = () => {
+    const humanChoice = prompt('Please make a choice between [rock, paper, scissors]');
+    if (humanChoice === null || humanChoice === '') return null;
+    return humanChoice;
+}
+
+const playRound = (humanChoice, computerChoice) => {
+    humanChoice = humanChoice.toLowerCase().trim();
+    if (humanChoice === computerChoice) {
+        return `It's a tie! You both chose${humanChoice}`
+    } else if 
+        (
+            (humanChoice === 'rock' && computerChoice === 'scissors') || 
+            (humanChoice === 'scissors' && computerChoice === 'paper') || 
+            (humanChoice === 'paper' && computerChoice === 'rock') 
+        ) 
+        {
+            humanScore++;
+            return `You win! ${humanChoice} beats ${computerChoice}`;
+    } else {
+        computerScore++
+        return `You lose! ${computerChoice} beats ${humanChoice}`
+    }
+}
+
+const humanSelection = getHumanChoice();
+if (humanSelection !== null) {
+    console.log(playRound(humanSelection, getComputerChoice()));
+}
